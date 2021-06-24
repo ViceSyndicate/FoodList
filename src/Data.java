735d995869bc8e29dca3d68f9323741210fd9279
Victor S <victor.skarerverket@gmail.com>
2021-06-24 02:47:01 +0200

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+
+public class Data {
+
+    public void getFoodArrayList() {
+        String fileName = "MyEmptyFile.txt";
+        try {
+            FileInputStream inputFile = new FileInputStream("FoodStorage.txt");
+            ObjectInputStream objectInput = new ObjectInputStream(inputFile);
+
+            while (true) {
+                Food food = (Food) objectInput.readObject();
+                System.out.println(food.getName());
+                System.out.println(food.isKetoFriendly());
+                System.out.println();
+            }
+        }
+        catch (EOFException eof) {
+            System.out.println("Reached end of file");
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Needs to be re-written so it doesn't overwrite previous item.
+    // Currently planning to get array of stored foods & appending
+    // the new food to the array, then saving the array to file again.
+    // Might also want to change file to .db instead. It's binary
+    // anyway it seems...
+    public Food addFood() {
+
+        String foodName;
+        boolean isKetoFriendly;
+
+        System.out.print("Enter the name of the food: ");
+        Scanner scannerInput = new Scanner(System.in);
+        foodName = scannerInput.nextLine();
+
+        System.out.println("\nIs it Keto friendly? Yes/No: ");
+        if (scannerInput.hasNext("y") || scannerInput.hasNext("yes"))
+            isKetoFriendly = true;
+        else if (scannerInput.hasNext("n") || scannerInput.hasNext("no"))
+            isKetoFriendly = false;
+        else
+            return null;
+        /*
+        scannerInput = new Scanner(System.in);
+        String userInput = scannerInput.toString().toLowerCase();
+
+        if (userInput == "y" || userInput == "yes")
+            isKetoFriendly = true;
+        else if (userInput == "n" || userInput == "no")
+            isKetoFriendly = false;
+        else {
+            System.out.println("Confusing user Input. ");
+            return null;
+        }
+         */
+        return new Food(foodName, isKetoFriendly);
+    }
+
+    public void storeFood(Food food) {
+        OutputStream ops = null;
+        ObjectOutputStream objOps = null;
+        try {
+            ops = new FileOutputStream("FoodStorage.txt");
+            objOps = new ObjectOutputStream(ops);
+            objOps.writeObject(food);
+            objOps.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try{
+                if(objOps != null) objOps.close();
+            } catch (Exception ex){
+                System.out.println(ex);
+            }
+        }
+
+    }
+
+    public void displayFoods(){
+
+        InputStream fileIs = null;
+        ObjectInputStream objIs = null;
+        try {
+            fileIs = new FileInputStream("FoodStorage.txt");
+            objIs = new ObjectInputStream(fileIs);
+
+            Food food = (Food) objIs.readObject();
+
+
+
+            System.out.println(food.getId());
+            System.out.println(food.getName());
+            System.out.println(food.isKetoFriendly());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(objIs != null) objIs.close();
+            } catch (Exception ex){
+
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // I feel like we should have a non static Menu function that can return an error.
+        // If it does, we should handle that error in main() and discard the return value.
+        // And call the Menu function again.
+
+        int userChoice = Menu();
+        while (userChoice == 10) {
+            System.out.println("Unexpected Input.");
+            userChoice = Menu();
+        }
+        System.out.println("Exiting Program.");
+    }
+
+    public static int Menu() {
+
+        Data data = new Data();
+        int selection;
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("---------------------\n");
+        System.out.println("1 - List All Foods");
+        System.out.println("2 - Add Food");
+        System.out.println("3 - Delete Item?");
+        System.out.println("0 - Quit\n");
+
+        if(!inputScanner.hasNextInt()) return 10;
+
+        selection = inputScanner.nextInt();
+        switch (selection) {
+            case 0: // Exits Program.
+                return 0;
+            case 1:
+                System.out.println("Food Menu.");
+                // Call Food Menu Function
+                data.getFoodArrayList();
+                return 1;
+            case 2:
+                // System.out.println("Add Food Function");
+                Food newFood = data.addFood();
+                if (newFood != null) data.storeFood(newFood);
+                return 2;
+            case 3:
+                System.out.println("Delete Food Function");
+                // Call Delete Food Function
+                return 3;
+            default:
+                return 10;
+        }
+    }
+}
